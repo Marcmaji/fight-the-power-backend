28e661a2141bc80a0eeb2393113c37f8936990f7
marcmaji <marcmaji97@gmail.com>
2023-12-24 12:02:34 +0100

    Initial commit

---
+++ src/main/java/com/marmaji/fightThePowerBackend/FightThePowerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.marmaji.fightThePowerBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FightThePowerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FightThePowerBackendApplication.class, args);
+	}
+
+}
